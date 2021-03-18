cd30e8a867e9d230e67df2c3c90100b4962f1b07
kevinpatel21 <78763175+kevinpatel21@users.noreply.github.com>
2021-03-18 18:39:56 -0500

    Product class

--- src/Product.java
+++ src/Product.java
@@ -7,39 +7,36 @@ public class Product implements Cloneable {
     Coordinate productLocation;
 
 
-    public Product(String inputName, double inputPrice){
+    public Product(String inputName, double inputPrice) {
         productName = inputName;
         productPrice = inputPrice;
         productTags = new ArrayList<String>();
         productLocation = new Coordinate(0, 0);
     }
 
-    public Product(){
+    public Product() {
         productName = "Unknown";
         productPrice = 0.00;
         productTags = new ArrayList<String>();
         productLocation = new Coordinate(0, 0);
     }
 
-    public Product clone()
-    {
-        try{
+    public Product clone() {
+        try {
             Product ProductClone = (Product) super.clone();
             ProductClone.productTags = (ArrayList<String>) productTags.clone();
 
             return ProductClone;
-        }
-        catch(CloneNotSupportedException e){
+        } catch (CloneNotSupportedException e) {
             return null;
         }
     }
 
-    public String getProductName(){
+    public String getProductName() {
         return productName;
     }
 
-    public ArrayList<String> getProductTags()
-    {
+    public ArrayList<String> getProductTags() {
         return (ArrayList<String>) productTags.clone();
     }
 
@@ -52,21 +49,40 @@ public class Product implements Cloneable {
         productTags.add(inputTag);
     }
 
-    public void setProductTags(ArrayList<String> inputTags)
-        {}
 
-    public Double getProductPrice(){
+
+
+
+    public Double getProductPrice() {
         return productPrice;
     }
 
-    public void setProductName(String inputName){
+    public void setProductName(String inputName) {
         productName = inputName;
     }
 
-    public void setProductPrice(Double inputPrice){
+    public void setProductPrice(Double inputPrice) {
         productPrice = inputPrice;
     }
 
+    public void setProductLocation(Coordinate inputLocation)
+    {
+        productLocation = inputLocation;
+    }
 
-    //a few methods still need to be added, refer to the design docs for more info
+    public Coordinate getProductLocation()
+    {
+        return productLocation;
+    }
+
+    public void printProductInfo()
+    {
+        System.out.println("Product Name: " + productName);
+        System.out.println("Product Price: "+ productPrice);
+        System.out.println("Product Tags: " + productTags);
+        System.out.println("Product Location: " + productLocation);
+    }
+
+
+
 }
