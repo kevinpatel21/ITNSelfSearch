33e929ea080db059796a0522c71bb47b7c2433c7
Harrison Matthews <hmatthews070@gmail.com>
2021-03-18 18:57:54 -0500

    Updated NameFilter file

--- src/NameFilter.java
+++ src/NameFilter.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 public class NameFilter
 {
     /**
+     * MUST use validProduct() function before calling this function
      * Function used to retrieve a Product in database by name.
      * @param inputName Name of Product that will be retrieved
      * @param d Needs access to the database so that we can use getProductCatalogue()
@@ -13,22 +14,15 @@ public class NameFilter
     {
         Product p = null;
 
-        // If the product we are searching for is in the database then return the product
-        if(d.validProductName(inputName) == true)
+        // Search for the product in the ProductCatalogue
+        for(Product product: d.getProductCatalogue())
         {
-            for(Product product: d.getProductCatalogue())
+            // When we find it we put the product into P then return it
+            if(product.getProductName().compareTo(inputName) == 0)
             {
-                if(product.getProductName().compareTo(inputName) == 0)
-                {
                     p = product;
                     break;
-                }
             }
-        }
-        // If the product is not in the database then return a empty product
-        else
-        {
-            return p.clone();
         }
 
         return p.clone();
