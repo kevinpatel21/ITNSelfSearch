f9a8af1d3e0a4458645f5787d150d3c403ac6aac
MPKellyy <cybernationnews@gmail.com>
2021-03-18 19:17:40 -0500

    Replacing main file

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+public class main {
     public static void main(String[] args){
         //Creating Database
         System.out.print("Enter file path: ");
