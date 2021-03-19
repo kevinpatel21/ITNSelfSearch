4601a1e278c9bde876945e8713c72409fb936932
Harrison Matthews <hmatthews070@gmail.com>
2021-03-18 21:27:48 -0500

    Made changes to how the tag filter interacts with the list

--- src/TagFilter.java
+++ src/TagFilter.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 public class TagFilter
 {
 
+
+
+
+
     /**
      * Pre: validProductName MUST be called first to check if Product is in the database BEFORE retrieving
      * Function used to retrieve multiple products by an input of tags
@@ -17,13 +21,36 @@ public class TagFilter
         //ArrayList that will hold related Products containing specified tags
         ArrayList<Product> matchingProducts = new ArrayList<Product>();
 
+        int productCounter = 0;
+
+
+
         // Search the catalogue for any products that contains all input tags and add it to matchingProducts list
         for (Product product: d.getProductCatalogue())
         {
-            if (product.getProductTags().containsAll(inputTags))
+            // We need to set the ProductTagNumber each time we get a new product for our loops
+            product.setProductTagNumber();
+            productCounter = product.getProductTagNumber();
+
+            /*if(product.getProductTags().containsAll(inputTags))
             {
                 matchingProducts.add(product.clone());
+            } */
+
+            // You need to iterate through all input tags to see if one matches with the ProductTags List
+            for(int i = 0; i < productCounter; i++)
+            {
+                for(int j = 0; j < inputTags.size(); j++)
+                {
+                    if(product.getProductTags().get(i).equals(inputTags.get(j)))
+                    {
+                        matchingProducts.add(product.clone());
+                    }
+                }
             }
+
+            //System.out.println(product.getProductTags());
+
         }
         return matchingProducts;
     }
