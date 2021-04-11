2507ed5686c81df15ce862534d522830f45aba49
Harrison Matthews <hmatthews070@gmail.com>
2021-04-11 14:34:47 -0500

    Controls tagMenu

--- src/tagMenuController.java
+++ src/tagMenuController.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class tagMenuController
@@ -8,6 +11,10 @@ public class tagMenuController
     private Product retrievedProduct;
     private ArrayList<String> inputTags;
     private TagFilter tagfilter;
+    private ArrayList<Product> retrievedProducts;
+
+    private final ArrayList<ChangeListener> searchListener = new ArrayList<ChangeListener>();//ArrayList of listeners
+    private final ArrayList<ChangeListener> backListener = new ArrayList<ChangeListener>();//ArrayList of listeners
 
 
 
@@ -17,43 +24,84 @@ public class tagMenuController
         inputTags = new ArrayList();
         tagfilter = tf;
         database = d;
+        retrievedProducts = new ArrayList();
     }
 
-   public void initController()
-   {
-       //ActionListeners for when A button is clicked
-       menu.getSearch().addActionListener(e -> search());
+    public void initController()
+    {
+        //ActionListeners for when A button is clicked
+        menu.getSearch().addActionListener(e -> search());
+        menu.getCancel().addActionListener(e -> cancel());
 
-   }
+    }
 
-   private void search()
-   {
-      int[] indices = menu.getListOfButtons().getSelectedIndices();
-       ArrayList<Product> retrievedProducts = new ArrayList<Product>();
+    private void cancel()
+    {
+        ChangeEvent backButtonSelected = new ChangeEvent(this);
+        for(ChangeListener listener: backListener)
+        {
+            listener.stateChanged(backButtonSelected);
+        }
+    }
 
-      // This For loop adds the selected indices into the inputTags
-      for(int i = 0; i < indices.length; i++)
-      {
-          inputTags.add(menu.getUniqueTags().get(indices[i]));
-      }
+    private void search()
+    {
+        int[] indices = menu.getListOfButtons().getSelectedIndices();
 
-      retrievedProducts = tagfilter.retrieveByTags(inputTags, database);
+        if(menu.getListOfButtons().isSelectionEmpty())
+        {
+            JOptionPane.showMessageDialog(null, "You have not made any selections");
+        }
+        else
+        {
+            // This For loop adds the selected indices into the inputTags
+            for(int i = 0; i < indices.length; i++)
+            {
+                inputTags.add(menu.getUniqueTags().get(indices[i]));
+            }
+
+            retrievedProducts = tagfilter.retrieveByTags(inputTags, database);
+
+            // Send the retrievedProducts list to the productSelection screen
+
+            for (Product product: retrievedProducts)
+            {
+                System.out.print(product.getProductName() + ": " + product.getProductPrice());
+
+                for(String tag: product.getProductTags())
+                {
+                    System.out.print(", " + tag);
+                }
+
+                System.out.println();
+            }
+
+            ChangeEvent searchSelected = new ChangeEvent(this);
+            for(ChangeListener listener: searchListener){
+                listener.stateChanged(searchSelected);
+            }
+
+            // We need to clear this array after every search
+            inputTags.clear();
+        }
 
 
-       for (Product product: retrievedProducts)
-       {
-           System.out.print(product.getProductName() + ": " + product.getProductPrice());
 
-           for(String tag: product.getProductTags())
-           {
-               System.out.print(", " + tag);
-           }
+    }
 
-           System.out.println();
-       }
+    public void addBackListener(ChangeListener newListener)
+    {
+        backListener.add(newListener);
+    }
 
-      // We need to clear this array after every search
-      inputTags.clear();
+    public void addSearchListener(ChangeListener newListener)
+    {
+        searchListener.add(newListener);
+    }
 
-   }
+    public ArrayList<Product> getRetrievedProducts()
+    {
+        return retrievedProducts;
+    }
+
 }
