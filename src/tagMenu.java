f92ff1e8873b01a5779e38958f79a7f0407c06de
Harrison Matthews <hmatthews070@gmail.com>
2021-04-13 13:44:18 -0500

    Made several comments to my code

--- src/tagMenu.java
+++ src/tagMenu.java
@@ -8,71 +8,46 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+/**
+ * This class is used for the view of our tagMenu GUI
+ */
 public class tagMenu extends JPanel
 {
+    // Declare our ArrayList for to store the uniqueTags
     private ArrayList<String> uniqueTags;
 
-    private ArrayList<JToggleButton> arrayButtons;
-    private JToggleButton button;
-    private JFrame frame2;
+    // Declare our two JButtons for our buttonPanel, our JList for our tags, and our tag for the name
+    private JButton search;
+    private JButton cancel;
+    private JList listOfButtons;
+    private JLabel tag;
 
+    // Declare our two JPanels for our JButtons and JList
     private JPanel buttonPanel;
     private JPanel listPanel;
 
+    // Declare our scrollPane for listPanel, and our Database that is needed to get the uniqueTags
     private JScrollPane scrollPane;
-    private String text;
     private Database d;
-    private JLabel tag;
-    private JButton search;
-    private JButton cancel;
-    private JTextArea textArea;
 
-    // Button listener arrays
-    private final ArrayList<ChangeListener> cancelListener = new ArrayList<ChangeListener>();//ArrayList of listeners
-
-
-    private JList listOfButtons;
-
+    /**
+     * This is the constructor for our tagMenu GUI.
+     * Calls createPanel to get the View
+     * @param data takes in the databse and puts the values into d
+     */
     public tagMenu(Database data)
     {
         d = data;
-        System.out.println("In tagMenu");
-        setButtons();
         createPanel();
     }
 
-    public void setButtons()
-    {
-        // Create the objects
-        arrayButtons = new  ArrayList();
-        uniqueTags = new ArrayList();
-
-        // We need to get the UNIQUE tags from database
-        uniqueTags = d.getStoreTags();
-
-        // This loop goes through all of the tags and creates a JToggleButton for them.
-        for(int i = 0; i < d.getStoreTagCounter(); i++)
-        {
-            text = uniqueTags.get(i);
-            button = new JToggleButton(text);
-            button.setMaximumSize(new Dimension(150,100));
-            arrayButtons.add(button);
-
-        }
-    }
-
+    /**
+     * This function creates the view that you see when you go into the tag menu
+     */
     public void createPanel()
     {
-        /*// Set up thr frame with a few settings
-        frame2 = new JFrame();
-        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame2.setSize(600, 400);
-        frame2.setResizable(true);
-        frame2.setLocationRelativeTo(null);
-        frame2.setVisible(true);
-        frame2.setTitle("Tag Menu"); */
 
-        // Declare our different panels needed. We need a total of 3
+        // Declare our different panels needed. We need a total of 3 buttonPanel, listPanel, and the panel that is being extended
         buttonPanel = new JPanel();
         listPanel = new JPanel();
 
@@ -81,76 +56,77 @@ public class tagMenu extends JPanel
         buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
         this.setLayout(new BorderLayout());
 
+        // Declare our Buttons, JList and our unique tags array list to store the tags into
+        uniqueTags = new ArrayList();
+        uniqueTags = d.getStoreTags();
         listOfButtons = new JList(uniqueTags.toArray());
         tag = new JLabel("Tag Menu");
         search = new JButton("Search");
         cancel = new JButton("Cancel");
 
+        // Set up the listOfButtons JList to multiple_interval selection and set the font
         listOfButtons.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
         listOfButtons.setFont(new Font("Serif", Font.ITALIC, 14));
+        listOfButtons.setLayoutOrientation(JList.VERTICAL);
+
+        // Declare the scroll Pane and set the viewport to the JList. Also set the preferred size
         scrollPane = new JScrollPane();
         scrollPane.setViewportView(listOfButtons);
-        listOfButtons.setLayoutOrientation(JList.VERTICAL);
         scrollPane.setPreferredSize(new Dimension(570,300));
 
+        // Now that the scrollPane is set up add it to the listPanel
+        listPanel.add(scrollPane);
 
+        // Set the buttonPanel settings so that the buttons appear in the bottom right
         buttonPanel.setBorder(BorderFactory.createEmptyBorder(0,10,10,10));
         buttonPanel.add(Box.createHorizontalGlue());
         buttonPanel.add(search);
         buttonPanel.add(Box.createRigidArea(new Dimension(10,0)));
         buttonPanel.add(cancel);
 
-        listPanel.add(scrollPane);
-
+        // Now that the panels are setup add them to the extended JPanel and determine where on the panel they will go
         this.add(listPanel, BorderLayout.CENTER);
         this.add(buttonPanel, BorderLayout.PAGE_END);
 
-        //frame2.add(this);
+        // Set the background color
         this.setBackground(Color.LIGHT_GRAY);
-
-        // Button Listeners
-        cancel.addActionListener(new ActionListener()
-        {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ChangeEvent mainSelected = new ChangeEvent(this);
-                for(ChangeListener listener: cancelListener){
-                    listener.stateChanged(mainSelected);
-                }
-            }
-        });
 
     }
 
+    /**
+     * This function gets us our private JList variable
+     * @return listOfButtons
+     */
     public JList getListOfButtons()
     {
         return listOfButtons;
     }
 
+    /**
+     * This function gets us our private button search
+     * @return search
+     */
     public JButton getSearch()
     {
         return search;
     }
 
+    /**
+     * This function gets us our private button cancel
+     * @return cancel
+     */
     public JButton getCancel()
     {
         return cancel;
     }
 
+    /**
+     * This function gets us our private ArrayList uniqueTags
+     * @return uniqueTags
+     */
     public ArrayList<String> getUniqueTags()
     {
         return uniqueTags;
     }
-
-    /**
-     * Function used to determine if user clicks on main menu
-     * @param newListener an input listener
-     */
-    public void addCancelListener(ChangeListener newListener)
-    {
-        cancelListener.add(newListener);
-    }
-
-
 
 }
